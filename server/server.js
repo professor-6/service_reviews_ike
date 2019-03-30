d650801cdbb2b023f1deba50b3fe4aff1f0cebba
Ike Okonkwo <paschal83@gmail.com>
2019-03-29 17:35:31 -0700

    Added three routes::/data/ratings_ambience, /data/reviewer_info, /data/reviews to facilitate seeding data into DB - probably need to refactor these to avoid using Postman to populate my DB

---
+++ server/server.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const db = require('../database/db');
+// const dataReadFromDisk = require('../database/data');
+
+const app = express();
+const PORT = 4000;
+
+// app.use(bodyParser.json());
+app.use(bodyParser.json({limit: '50mb'}));
+
+app.listen(PORT, function(){
+  console.log(`Listening on port : ${PORT}`);
+})
+
+// ###################Need Refactor
+app.post('/data/ratings_ambience', function(req, res){
+  var queryString = `INSERT INTO ratings_ambience SET ?`;
+  for(var i = 0  ; i < req.body.length ; i++) {
+    db.query(queryString, req.body[i], function (err, results){
+      if (err){
+        console.error('ERROR : Could not seed database');
+        throw err;
+      }
+      console.log('SUCCESS : inserted records into database : ');
+      res.send(results);
+    });
+  }
+});
+
+app.post('/data/reviewer_info', function(req, res){
+  var queryString = `INSERT INTO reviewer_info SET ?`;
+  for(var i = 0  ; i < req.body.length ; i++) {
+    db.query(queryString, req.body[i], function (err, results){
+      if (err){
+        console.error('ERROR : Could not seed database');
+        throw err;
+      }
+      console.log('SUCCESS : inserted records into database : ');
+      res.send(results);
+    });
+  }
+});
+
+app.post('/data/reviews', function(req, res){
+  var queryString = `INSERT INTO reviews SET ?`;
+  for(var i = 0  ; i < req.body.length ; i++) {
+    db.query(queryString, req.body[i], function (err, results){
+      if (err){
+        console.error('ERROR : Could not seed database');
+        throw err;
+      }
+      console.log('SUCCESS : inserted records into database : ');
+      res.send(results);
+    });
+  }
+});
+
