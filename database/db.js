0153499710f1e65536b3f6809019cdb3d428692c
Ike Okonkwo <paschal83@gmail.com>
2019-03-29 17:44:50 -0700

    Add DB setup credentials

---
+++ database/db.js
@@ -0,0 +1,21 @@
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host : 'localhost',
+  user : 'root',
+  password : 'root',
+  database : 'opentable'
+
+})
+
+connection.connect(function(err){
+  if(err) {
+    console.error('ERROR connecting to database');
+    return;
+  } else {
+    console.log(`Connection to MySQL established at : ${connection.threadId}`);
+  }
+
+});
+
+module.exports = connection;
