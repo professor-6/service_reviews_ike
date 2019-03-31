644aeaa90ca07fa6e29e93d0cc6090e95b86ed07
Ike Okonkwo <paschal83@gmail.com>
2019-03-31 01:47:23 -0700

    Added some more GET end-pint to retrieve data from each table -> /api/reviewer_info/:id, /api/ratings_ambience/:id, /api/reviews/:id. Also added the main interface tot he proxy : /api/restaurants/:id

--- server/server.js
+++ server/server.js
@@ -14,52 +14,8 @@ app.listen(PORT, function(){
   console.log(`Listening on port : ${PORT}`);
 })
 
-// ###################Need Refactor
-app.post('/data/ratings_ambience', function(req, res){
-  var queryString = `INSERT INTO ratings_ambience SET ?`;
-  for(var i = 0  ; i < req.body.length ; i++) {
-    db.query(queryString, req.body[i], function (err, results){
-      if (err){
-        console.error('ERROR : Could not seed database');
-        throw err;
-      }
-      console.log('SUCCESS : inserted records into database : ');
-      res.send(results);
-    });
-  }
-});
-
-app.post('/data/reviewer_info', function(req, res){
-  var queryString = `INSERT INTO reviewer_info SET ?`;
-  for(var i = 0  ; i < req.body.length ; i++) {
-    db.query(queryString, req.body[i], function (err, results){
-      if (err){
-        console.error('ERROR : Could not seed database');
-        throw err;
-      }
-      console.log('SUCCESS : inserted records into database : ');
-      res.send(results);
-    });
-  }
-});
-
-app.post('/data/reviews', function(req, res){
-  var queryString = `INSERT INTO reviews SET ?`;
-  for(var i = 0  ; i < req.body.length ; i++) {
-    db.query(queryString, req.body[i], function (err, results){
-      if (err){
-        console.error('ERROR : Could not seed database');
-        throw err;
-      }
-      console.log('SUCCESS : inserted records into database : ');
-      res.send(results);
-    });
-  }
-});
-
-
-// get all records
-app.get('/data/reviewer_info', function(req, res){
+// get all records from table::reviewer_info
+app.get('/api/reviewer_info', function(req, res){
   var queryString = `SELECT * FROM reviewer_info`;
   db.query(queryString, function (err, results){
     if (err){
@@ -71,8 +27,8 @@ app.get('/data/reviewer_info', function(req, res){
   });
 });
 
-// get on record
-app.get('/data/reviewer_info/:id', function(req, res){
+// get a record from table::reviewer_info
+app.get('/api/reviewer_info/:id', function(req, res){
   var queryString = `SELECT * FROM reviewer_info WHERE id = ?`;
   db.query(queryString, req.params.id, function (err, results){
     if (err){
@@ -84,3 +40,48 @@ app.get('/data/reviewer_info/:id', function(req, res){
   });
 });
 
+
+// get a record from table::ratings_ambience
+app.get('/api/ratings_ambience/:id', function(req, res){
+  var queryString = `SELECT * FROM ratings_ambience WHERE id = ?`;
+  db.query(queryString, req.params.id, function (err, results){
+    if (err){
+      console.error('ERROR : Could not seed database');
+      throw err;
+    }
+    console.log(`SUCCESS : Retrieved records from  database : opentable `)
+    res.send(results);
+  });
+});
+
+
+// get a record from table::reviews
+app.get('/api/reviews/:id', function(req, res){
+  var queryString = `SELECT * FROM reviews WHERE id = ?`;
+  db.query(queryString, req.params.id, function (err, results){
+    if (err){
+      console.error('ERROR : Could not seed database');
+      throw err;
+    }
+    console.log(`SUCCESS : Retrieved records from  database : opentable `)
+    res.send(results);
+  });
+});
+
+
+// get all data for one restaurant
+app.get('/api/restaurants/:id', function(req, res){
+  var queryString = `SELECT * FROM ratings_ambience a
+                      JOIN reviewer_info  b ON a.id = b.id
+                      JOIN reviews c ON a.id = c.restaurant_id
+                      WHERE id = ?`;
+
+  db.query(queryString, req.params.id, function (err, results){
+    if (err){
+      console.error('ERROR : Could not seed database');
+      throw err;
+    }
+    console.log(`SUCCESS : Retrieved records from  database : opentable `)
+    res.send(results);
+  });
+});
