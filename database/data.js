9788f75c0e050ec9a6563e6369442ad686b77695
Ike Okonkwo <paschal83@gmail.com>
2019-03-31 01:35:03 -0700

    Finished function::dataWriteToDisk which generates fake data with faker module and writes generated files to disk

--- database/data.js
+++ database/data.js
@@ -14,19 +14,20 @@ var generateRating = function(max){
 // generate data
 var data_ratings_ambience = function(){
   var ratings_ambience = {
-    restaurant_rating : generateRating(5),
-    food_rating : generateRating(5),
-    service_rating : generateRating(5),
-    ambience_rating : generateRating(5),
-    value_rating : generateRating(5),
-    no_of_reviews : faker.random.boolean(),
-    noise_level : generateRating(5),
-    percent_recommend :generateRating(100)
+    restaurant_name : '',
+    restaurant_rating : faker.random.number({"min" : 0, "max" : 5}),
+    food_rating : faker.random.number({"min" : 0, "max" : 5}),
+    service_rating : faker.random.number({"min" : 0, "max" : 5}),
+    ambience_rating : faker.random.number({"min" : 0, "max" : 5}),
+    value_rating : faker.random.number({"min" : 0, "max" : 5}),
+    no_of_reviews : faker.random.number({"min" :1, "max" : 1000}),
+    noise_level : faker.random.number({"min" : 0, "max" : 5}),
+    percent_recommend : faker.random.number({"min" : 0, "max" : 100}),
   };
   return ratings_ambience;
 };
 
-var  data_reviewer_info = function(id){
+var  data_reviewer_info = function(){
 
   var parse_date = function(){
     var date = faker.date.past();
@@ -34,19 +35,16 @@ var  data_reviewer_info = function(id){
   }
 
   var reviewer_info = {
-    restaurant_id : id,
     dined_at : parse_date(),
     reviewer_name : faker.internet.userName(),
     reviewer_city : faker.address.city(),
-    no_of_reviews : faker.random.number(),
+    no_of_reviews : faker.random.number({"min" : 5, "max" : 1000}),
     reviewer_picture : faker.image.avatar(),
     reviewer_VIP : (faker.random.boolean()).toString()
   };
 
   return reviewer_info;
 };
-
-// console.log(data_reviewer_info());
 
 
 var data_reviews = function(id0, id1){
@@ -58,8 +56,6 @@ var data_reviews = function(id0, id1){
   return review;
 };
 
-// console.log(data_reviews(1));
-
 
 var dataWriteToDisk = function(fileName, callback){
 
@@ -69,13 +65,13 @@ var dataWriteToDisk = function(fileName, callback){
   if (fileName === 'reviews') {
     // get 10 reviews per restaurant
     for ( var i = 0; i < 10 ; i++){
-      for (var j = 0; j <= noOfRestaurants ; j++) {
+      for (var j = 0; j < noOfRestaurants ; j++) {
         dataToStore.push(callback(j+1, i+1));
       }
     }
   } else {
       for (var i = 1; i <= noOfRestaurants ; i++) {
-        dataToStore.push(callback(i));
+        dataToStore.push(callback());
       }
   }
   fs.writeFile( __dirname + `/data/${fileName}.json`,
@@ -83,7 +79,7 @@ var dataWriteToDisk = function(fileName, callback){
     if (err) {
       throw err;
     } else {
-      console.log(`SUCCESS : File ${fileName} created`);
+      console.log(`SUCCESS : File /data/${fileName}.json created`);
     }
   })
 
@@ -93,86 +89,3 @@ dataWriteToDisk('ratings_ambience', data_ratings_ambience)
 dataWriteToDisk('reviewer_info', data_reviewer_info)
 dataWriteToDisk('reviews', data_reviews);
 
-
-
-// fs.readFile( __dirname + '/restaurants_id_name.csv', (err, data) => {
-//   if (err) {
-//     console.error(err)
-//     return
-//     }
-//   console.log(data)
-//   })
-
-// const results = [];
-
-// // fs.createReadStream(__dirname + '/restaurants_id_name.csv')
-// fs.createReadStream(__dirname + '/reviewer_info.csv')
-// .pipe(csv())
-// .on('data', (data) =>
-//     results.push(data))
-//     // console.log(data.id)
-//     // console.log(data.name)
-// .on('end', () => {
-//   console.log(results);
-//   // console.log(results[0].id);
-//   // console.log(results[0].name);
-
-// });
-
-// reviewer_info.json
-// reviews.json
-// ratings_ambience.json
-
-var dataReadFromDisk = function(fileName){
-  fs.readFile( __dirname + `/data/${fileName}.json`, (err, data) => {
-      if (err) {
-        console.error(' ERROR : Cannot read file');
-      }
-      var  student = JSON.parse(data);
-      // console.log(student)
-      console.log(student[0]);
-      // return JSON.parse(data);
-      return student[0];
-      // console.log(student[1]);
-
-  })
-};
-
-// dataReadFromDisk('reviewer_info');
-// reviewer_info.json
-// reviews.json
-// ratings_ambience.json
-
-module.exports = dataReadFromDisk;
-
-
-
-
-
-// // table - ratings_ambience
-// id
-// restaurant_id
-// reviews - faker.lorem.paragraph()
-
-
-// table - ratings_ambience
-// id
-// restaurant_name
-// restaurant_rating -  generateRating(5);
-// food_rating -  generateRating(5);
-// service_rating -  generateRating(5);
-// ambience_rating -  generateRating(5);
-// value_rating -  generateRating(5);
-// no_of_reviews -  faker.random.boolean()
-// noise_level -  generateRating(5);
-// percent_recommend - generateRating(100);
-
-// // table - reviewer_info
-// id
-// restaurant_id
-// dined_at - faker.date.past()
-// reviewer_name - faker.internet.userName()
-// reviewer_city - faker.address.city()
-// no_of_reviews -  faker.random.boolean()
-// reviewer_picture  - faker.image.avatar()
-// reviewer_VIP - faker.random.boolean()
