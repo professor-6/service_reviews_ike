e26cbb0610035fb3d7695ee6b5b28e7dcc0b0b2a
Ike Okonkwo <paschal83@gmail.com>
2019-04-04 14:39:36 -0700

    Main App Component

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import $ from "jquery";
 
-import info from './components/info.jsx'
+import Summary from './components/reviews_summary.jsx'
 import Reviews from './components/reviews.jsx'
 
 
@@ -10,9 +10,34 @@ class App extends React.Component {
 
   render () {
     return (
+
       <div>
-        <h1> Hello React World </h1>
-      </div>
+         <div className="app">
+           <Summary />
+           {/* <div className="category">
+             <h3>Budget Categories</h3>
+             <CategoryList categoryList={this.state.cat_list} />
+               <div className="category-form">
+                 <form onSubmit={this.handleSubmit}
+                       onClick={this.someMethod}>
+                     <div className="category-input">
+                     <input
+                       type="text"
+                       placeholder="Budget Category"
+                       value={this.state.value}
+                       onChange={this.handleChange}
+                     />
+                     <input
+                       type="number"
+                       placeholder="Target Budget"
+                     />
+                     </div>
+                     <button onClick={this.someMethod}> Add </button>
+                 </form>
+               </div>
+           </div> */}
+         </div>
+       </div>
     )
   }
 }
@@ -20,3 +45,4 @@ class App extends React.Component {
 
 
 ReactDOM.render(<App />, document.getElementById('app'));
+
