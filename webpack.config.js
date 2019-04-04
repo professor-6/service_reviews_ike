82f85c99f76675b6e05107f02d63571f3c05b559
Ike Okonkwo <paschal83@gmail.com>
2019-04-04 14:30:02 -0700

    Made some changes to add img-loader

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,14 @@ module.exports = {
         query: {
           presets: ['react', 'es2015']
         }
+      },
+
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [
+          'url-loader?limit=10000',
+          'img-loader'
+        ]
       }
     ]
   }
