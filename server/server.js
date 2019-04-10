40a379332885308e42657a9dcac92c3ca48b4e31
Ike Okonkwo <paschal83@gmail.com>
2019-04-09 17:47:45 -0700

    Added fall back API route to render eacd id

--- server/server.js
+++ server/server.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
+const path  = require('path');
 const db = require('../database/db');
 
 const app = express();
 const PORT = process.env.PORT || 3002;
 
-// app.use(bodyParser.json());
 app.use(bodyParser.json({limit: '50mb'}));
-
 app.use(express.static(__dirname + '/../client/dist'));
 
 app.listen(PORT, function(){
@@ -17,7 +16,7 @@ app.listen(PORT, function(){
 
 
 // get a record from table::ratings_ambience
-app.get('/api/ratings_ambience/:id', function(req, res){
+app.get('/ratings_ambience/:id', function(req, res){
   var queryString = `SELECT * FROM ratings_ambience WHERE id = ?`;
   db.query(queryString, req.params.id, function (err, results){
     if (err){
@@ -29,39 +28,8 @@ app.get('/api/ratings_ambience/:id', function(req, res){
   });
 });
 
-
-// // get a record from table::reviews
-// app.get('/api/reviews/:id', function(req, res){
-//   var queryString = `SELECT * FROM reviews WHERE id = ?`;
-//   db.query(queryString, req.params.id, function (err, results){
-//     if (err){
-//       console.error('ERROR : Could not seed database');
-//       throw err;
-//     }
-//     console.log(`SUCCESS : Retrieved records from  database : opentable `)
-//     res.send(results);
-//   });
-// });
-
-
-// // get all data for all restaurants
-// app.get('/api/restaurants/', function(req, res){
-//   var queryString = `SELECT * FROM ratings_ambience a
-//                       JOIN reviews b ON b.restaurant_id = a.id`;
-
-//   db.query(queryString, req.params.id, function (err, results){
-//     if (err){
-//       console.error('ERROR : Could not seed database');
-//       throw err;
-//     }
-//     console.log(`SUCCESS : Retrieved records from  database : opentable `)
-//     res.send(results);
-//   });
-// });
-
 // get all data for one restaurant
-app.get('/:id', function(req, res){
-  // console.log(req.url};
+app.get('/reviews/:id', function(req, res){
   var queryString = `SELECT * FROM ratings_ambience a
                       JOIN reviews b ON b.restaurant_id = a.id
                       WHERE b.restaurant_id = ?`;
@@ -73,10 +41,14 @@ app.get('/:id', function(req, res){
     }
     console.log(`SUCCESS : Retrieved records from  database : opentable `)
     res.send(results);
-    // res.status(200).end();
-
   });
 });
+
+// fallback route
+app.get('/:id', function(req, res){
+  res.sendFile(path.join(__dirname + '/../client/dist/index.html'));
+});
+
 
 //========= FILTERS =================
  // newest
